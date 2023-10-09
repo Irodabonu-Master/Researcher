a5a0fd28e7414e471d4c40ac75d9724cf7710ba1
Irodabonu <kamilovairodabonu@gmail.com>
2023-10-10 03:35:03 +0500

    good

---
+++ Js/indx.js
@@ -0,0 +1,11 @@
+"use strict";
+let personal_Info = {
+  name_my: "Irodabonu",
+  age_my: 18,
+  adres_em: "kamilovairodabonu@gmail.com",
+  city: "Tashkent",
+  postcode: 10077,
+};
+
+const l = `Iroda`;
+console.log(personal_Info.name_my);
